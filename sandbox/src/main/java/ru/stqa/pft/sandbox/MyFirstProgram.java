6f5df72b401141644f88786373025d47a5a6ba05
Liliia <liliya.mikhareva@yandex.ru>
2017-11-29 00:10:39 +0300

    Функция расчета расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,10 +12,19 @@ public class MyFirstProgram{
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+		
+		Point p1 = new Point(2, 3);
+		Point p2 = new Point(4, 5);
+		System.out.println("Расстояние между точками = " +distance(p1, p2));
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
 	}
 
+	public static double distance(Point p1, Point p2) {
+		double difX = p2.x - p1.x;
+		double difY = p2.y - p1.y;
+		return Math.sqrt(Math.pow(difX, 2) + Math.pow(difY, 2));
+	}
 }
