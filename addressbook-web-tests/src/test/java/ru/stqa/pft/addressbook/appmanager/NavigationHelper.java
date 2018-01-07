094dd73755656f1a8dee99ebee56924ed818ed3c
Liliia <liliya.mikhareva@yandex.ru>
2018-01-07 16:41:01 +0300

    Создание базового класса для вспомогательных функций TestHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,16 +3,15 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
-  public void gotoContactCreationPage() { wd.findElement(By.linkText("add new")).click(); }
+  public void gotoContactCreationPage() { click(By.linkText("add new")); }
 }
