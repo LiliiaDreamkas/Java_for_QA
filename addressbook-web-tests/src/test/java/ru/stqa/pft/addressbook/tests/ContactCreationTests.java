f1042a57f08f35313d04d857c67d9175adaeffab
Liliia <liliya.mikhareva@yandex.ru>
2018-01-31 01:03:09 +0300

    Тест на создание контакта со сравнением коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,16 +1,31 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase{
 
   @Test
   public void testContactCreation() {
-    app.getContactHelper().createContact(new ContactData("TestFName", "TestLastName",
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    ContactData contact = new ContactData("TestFirstName", "TestLastName",
             "Test address", "+7(000) 000-00-00", "+7 812 495-00-00",
-            "+7 812 311-00-00", "testEmail@gmail.com", "test1"));
+            "+7 812 311-00-00", "testEmail@gmail.com", "test1");
+    app.getContactHelper().createContact(contact);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
