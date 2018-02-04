ee2d1d3c54c3e21250dc11a90e9e74f9fa1d94da
Liliia <liliya.mikhareva@yandex.ru>
2018-02-04 22:06:26 +0300

    Тест модификации группы со сравнением сведений из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -26,6 +26,7 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -36,6 +37,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.FIREFOX)){
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary(properties.getProperty("web.firefoxPath", "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")));
@@ -66,4 +69,8 @@ public class ApplicationManager {
   }
 
   public ContactHelper contact() { return contactHelper;  }
+
+  public DbHelper db() {
+    return dbHelper;
+  }
 }
