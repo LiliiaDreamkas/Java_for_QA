cabe5774d2d51ede8c61ad5e6cb2f069d04a403b
Liliia <liliya.mikhareva@yandex.ru>
2018-02-04 14:32:49 +0300

    Генератор тестовых данных для контактов в формате JSON

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,15 +1,24 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 
 public class ContactData {
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String firstName;
+  @Expose
   private String lastName;
+  @Expose
   private String address;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String homePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String email;
   private String email2;
   private String email3;
