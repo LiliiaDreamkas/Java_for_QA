76baeaad56ad3f9cad46b4f7c04f16cd0b9f7cb2
Liliia <liliya.mikhareva@yandex.ru>
2018-02-07 16:32:04 +0300

    Передача файлов на удаленную машину

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -26,24 +26,16 @@ public class HttpSession {
   }
 
   public boolean login(String username, String password) throws IOException {
-    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php"); //"/login_password_page.php");
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
     List<NameValuePair> params = new ArrayList<NameValuePair>();
     params.add(new BasicNameValuePair("username", username));
-//    params.add(new BasicNameValuePair("password", password));
-//    params.add(new BasicNameValuePair("secure_session", "on"));
+    params.add(new BasicNameValuePair("password", password));
+    params.add(new BasicNameValuePair("secure_session", "on"));
     params.add(new BasicNameValuePair("return", "index.php"));
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = getTextFrom(response);
     return body.contains(String.format("<span class=\"italic\">%s</span>", username));
-//    return body.contains(String.format("<input type=\"hidden\" name=\"username\" value=\"%s\" />", username));
-
-//
-//
-//    post.setEntity(new UrlEncodedFormEntity(params));
-//    CloseableHttpResponse response = httpclient.execute(post);
-//    String body = getTextFrom(response);
-//    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 
   private String getTextFrom(CloseableHttpResponse response) throws IOException {
@@ -55,7 +47,7 @@ public class HttpSession {
   }
 
   public boolean isLoggedInAs(String username) throws IOException {
-    HttpGet get = new HttpGet(app.getProperty("web.baseURL") + "/index.php");
+    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
     CloseableHttpResponse response = httpclient.execute(get);
     String body = getTextFrom(response);
     return body.contains(String.format("<span class=\"italic\">%s</span>", username));
