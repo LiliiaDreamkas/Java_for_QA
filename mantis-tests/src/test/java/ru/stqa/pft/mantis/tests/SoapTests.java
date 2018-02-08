ac5133e04802219b1a68e9b55cc1e311a1029590
Liliia <liliya.mikhareva@yandex.ru>
2018-02-08 21:45:17 +0300

    создается баг в mantis

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -4,6 +4,9 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
@@ -11,20 +14,20 @@ import static org.testng.Assert.assertEquals;
 public class SoapTests extends TestBase {
 
     @Test
-    public void testGetProjects() {
+    public void testGetProjects() throws RemoteException, ServiceException, MalformedURLException {
         Set<Project> projects = app.soap().getProgect();
-        System.out.println(progects.size());
-        for (ProjectData project : projects) {
+        System.out.println(projects.size());
+        for (Project project : projects) {
             System.out.println(project.getName());
         }
     }
 
     @Test
-    public void testCreateIssue() {
+    public void testCreateIssue() throws MalformedURLException, RemoteException, ServiceException {
         Set<Project> projects = app.soap().getProgect();
         Issue issue = new Issue().withSummary("Test issue")
                 .withDescription("Test issue description").withProject(projects.iterator().next());
         Issue created = app.soap().addIssue(issue);
-        assertEquals(issue.getSummary(), created.getSummary())
+        assertEquals(issue.getSummary(), created.getSummary());
     }
 }
