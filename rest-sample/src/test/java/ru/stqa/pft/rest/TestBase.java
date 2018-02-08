957cc3906aea62f1979f4853f7cba80181d2ae02
Liliia <liliya.mikhareva@yandex.ru>
2018-02-09 02:01:38 +0300

    Задание 20

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -0,0 +1,53 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.SkipException;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class TestBase {
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+//    Set<Issue> allIssues = getIssues();
+//    Issue currentIssue = allIssues.iterator().next();
+//    String status = getIssueStatus(currentIssue.getId());
+    String status = getIssueStatus(issueId);
+    if(status.equals("Resolved") || status.equals("Closed")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  protected Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  protected Executor getExecutor() {
+    return Executor.newInstance().auth("28accbe43ea112d9feb328d2c00b3eed", "");
+  }
+
+  private String getIssueStatus(int issueId) throws IOException {
+    String json = getExecutor().execute(Request.Get(String.format("http://demo.bugify.com/api/issues/%s.json", issueId)))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("state_name").getAsString();
+  }
+}
